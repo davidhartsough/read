96236afb2173c3a2ccbebfc81b9718358ece745e
David Hartsough <hartsoughdavid@gmail.com>
2020-11-25 11:42:28 -0700

    switch to using nearly full html injection instead of just text content

--- src/App.js
+++ src/App.js
@@ -2,10 +2,120 @@ import React, { useState } from "react";
 import axios from "axios";
 
 const urlPattern = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)$/;
+const elementsToRemove = [
+  "script",
+  "style",
+  "area",
+  "audio",
+  "img",
+  "map",
+  "track",
+  "video",
+  "embed",
+  "iframe",
+  "object",
+  "param",
+  "picture",
+  "source",
+  "canvas",
+  "noscript",
+  "button",
+  "datalist",
+  "fieldset",
+  "form",
+  "input",
+  "label",
+  "legend",
+  "meter",
+  "optgroup",
+  "option",
+  "output",
+  "progress",
+  "select",
+  "textarea",
+  "menu",
+  "applet",
+  "bgsound",
+  "image",
+];
+const goodElements = [
+  "address",
+  "article",
+  "aside",
+  "footer",
+  "header",
+  "h1",
+  "h2",
+  "h3",
+  "h4",
+  "h5",
+  "h6",
+  "hgroup",
+  "main",
+  "nav",
+  "section",
+  "blockquote",
+  "dd",
+  "div",
+  "dl",
+  "dt",
+  "figcaption",
+  "figure",
+  "hr",
+  "li",
+  "ol",
+  "p",
+  "pre",
+  "ul",
+  "a",
+  "abbr",
+  "b",
+  "bdi",
+  "bdo",
+  "br",
+  "cite",
+  "code",
+  "data",
+  "dfn",
+  "em",
+  "i",
+  "kbd",
+  "mark",
+  "q",
+  "s",
+  "samp",
+  "small",
+  "span",
+  "strong",
+  "sub",
+  "sup",
+  "time",
+  "u",
+  "var",
+  "wbr",
+  "del",
+  "ins",
+  "caption",
+  "col",
+  "colgroup",
+  "table",
+  "tbody",
+  "td",
+  "tfoot",
+  "th",
+  "thead",
+  "tr",
+  "details",
+  "dialog",
+  "summary",
+  "slot",
+  "template",
+];
 
 export default function App() {
   const [url, setUrl] = useState("");
-  const [text, setText] = useState("");
+  const [html, setHtml] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
   const [title, setTitle] = useState("");
   const [inputError, setInputError] = useState(false);
   function handleChange({ target: { value } }) {
@@ -18,27 +128,39 @@ export default function App() {
       data.toLowerCase().indexOf("<!doctype") === -1
     ) {
       setTitle("Error: Sorry, wrong type of webpage.");
-      setText("Please enter a URL for an HTML page.");
+      setErrorMessage("Please enter a URL for an HTML page.");
       return;
     }
     const doc = new DOMParser().parseFromString(data, "text/html");
     setTitle(doc.title);
     const { body } = doc;
-    Array.from(body.getElementsByTagName("script")).forEach((se) => {
-      se.remove();
+    elementsToRemove.forEach((tag) => {
+      Array.from(body.getElementsByTagName(tag)).forEach((e) => e.remove());
     });
-    Array.from(body.getElementsByTagName("style")).forEach((se) => {
-      se.remove();
+    Array.from(body.getElementsByTagName("*")).forEach((e) => {
+      e.removeAttribute("style");
+      if (!goodElements.includes(e.tagName.toLowerCase())) e.remove();
     });
-    let html = body;
+    const { origin } = new URL(url.trim());
+    Array.from(body.getElementsByTagName("a")).forEach((e) => {
+      e.setAttribute("target", "_blank");
+      e.setAttribute("rel", "noreferrer");
+      const href = e.getAttribute("href");
+      if (href && href.startsWith("/")) {
+        e.setAttribute("href", `${origin}${href}`);
+      }
+    });
+    const content = doc.getElementById("content");
+    let newHtml = content || body;
     if (data.includes("<article")) {
-      [html] = body.getElementsByTagName("article");
+      [newHtml] = body.getElementsByTagName("article");
     }
-    setText(html.innerText);
+    setHtml(newHtml.innerHTML);
   }
   async function read() {
     setTitle("Loading ...");
-    setText("");
+    setErrorMessage(null);
+    setHtml(null);
     try {
       const { data } = await axios.get(url.trim());
       parseHtml(data);
@@ -58,7 +180,7 @@ export default function App() {
         // eslint-disable-next-line no-console
         console.log(errorResponse);
         setTitle("Uh oh. Bummer. Can't get the text from that web page.");
-        setText("Are you sure the URL is correct?");
+        setErrorMessage("Are you sure the URL is correct?");
       }
     }
   }
@@ -66,6 +188,7 @@ export default function App() {
     e.preventDefault();
     if (!inputError) read();
   }
+  const createMarkup = () => ({ __html: html });
   return (
     <>
       <form onSubmit={handleSubmit}>
@@ -80,10 +203,10 @@ export default function App() {
           READ
         </button>
       </form>
-      <article id="read">
-        <h1 id="title">{title}</h1>
-        <p id="text">{text}</p>
-      </article>
+      {title.length > 0 && <h1 id="title">{title}</h1>}
+      {errorMessage && <p>{errorMessage}</p>}
+      {/* eslint-disable-next-line react/no-danger */}
+      <article dangerouslySetInnerHTML={createMarkup()} />
     </>
   );
 }
